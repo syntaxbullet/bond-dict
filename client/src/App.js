47500f3f0a32a463f0ab1cec8d206fd66c052855
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-10-13 20:55:25 -0400

    it works! i think?

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import {
     ChakraProvider,
     Box,
@@ -8,6 +8,7 @@ import {
     Grid,
     Button,
     theme,
+    Container,
     useColorModeValue,
 } from '@chakra-ui/react';
 import Pattern from './components/Pattern';
@@ -17,6 +18,8 @@ function App() {
     const [patternData, setPatternData] = useState([]);
     const [creationMode, setCreationMode] = useState(false);
     const [isRecording, setRecording] = useState(false);
+    const [time, setTime] = useState();
+    const btnRef = useRef(null);
 
     useEffect(() => {
         fetch('http://192.168.178.71:5000/patterns')
@@ -26,15 +29,32 @@ function App() {
                 setCreationMode(false);
                 setRecording(false);
             });
-    }, []);
-    function handleRecording() {
-        // handle recording here
-    }
 
-    if (creationMode) {
-        return (
-            <ChakraProvider theme={theme}>
-                <Box textAlign="center" fontSize="xl">
+        if (!btnRef.current) return;
+
+        const mouseDown = () => {
+            if (time) setPatternData([...patternData, Date.now() - time]);
+
+            setTime(Date.now());
+        };
+        const mouseUp = () => {
+            setPatternData([...patternData, Date.now() - time]);
+            setTime(Date.now());
+        };
+
+        btnRef.current.addEventListener('mousedown', mouseDown);
+        btnRef.current.addEventListener('mouseup', mouseUp);
+
+        return () => {
+            btnRef.current.removeEventListener('mousedown', mouseDown);
+            btnRef.current.removeEventListener('mouseup', mouseUp);
+        };
+    }, [btnRef, creationMode, patternData, time]);
+
+    return (
+        <ChakraProvider theme={theme}>
+            <Box textAlign="center" fontSize="xl">
+                {creationMode ? (
                     <Grid minH="100vh" p={3}>
                         <ColorModeSwitcher justifySelf="flex-end" />
                         <VStack spacing={8}>
@@ -46,13 +66,14 @@ function App() {
                                 pattern. Once you are done, you can either save
                                 your pattern or start again.
                             </Text>
+                            <Text>{JSON.stringify(patternData, null, 2)}</Text>
                             <svg
                                 width="287"
                                 height="119"
                                 viewBox="0 0 287 119"
                                 fill="none"
                                 xmlns="http://www.w3.org/2000/svg"
-                                onClick={() => handleRecording()}
+                                ref={btnRef}
                             >
                                 <rect
                                     x="2"
@@ -76,47 +97,43 @@ function App() {
                             </svg>
                         </VStack>
                     </Grid>
-                </Box>
-            </ChakraProvider>
-        );
-    } else {
-        return (
-            <ChakraProvider theme={theme}>
-                <Box textAlign="center" fontSize="xl">
-                    <Grid minH="100vh" p={3}>
-                        <ColorModeSwitcher justifySelf="flex-end" />
-                        <VStack spacing={8}>
-                            <Heading as="h1" size="3xl">
-                                Our Bond Touch Dictionary
-                            </Heading>
-                            <Text fontSize="md" color="GrayText" maxW="75%">
-                                A simple web application that allows us to add
-                                patterns and meanings to our bond touch
-                                dictionary.
-                            </Text>
-                            <Button
-                                colorScheme="blue"
-                                size="md"
-                                onClick={() => {
-                                    setCreationMode(true);
-                                }}
-                            >
-                                Add a Pattern
-                            </Button>
+                ) : (
+                    <>
+                        <Grid minH="100vh" p={3}>
+                            <ColorModeSwitcher justifySelf="flex-end" />
+                            <VStack spacing={8}>
+                                <Heading as="h1" size="3xl">
+                                    Our Bond Touch Dictionary
+                                </Heading>
+                                <Text fontSize="md" color="GrayText" maxW="75%">
+                                    A simple web application that allows us to
+                                    add patterns and meanings to our bond touch
+                                    dictionary.
+                                </Text>
+                                <Button
+                                    colorScheme="blue"
+                                    size="md"
+                                    onClick={() => {
+                                        setCreationMode(true);
+                                    }}
+                                >
+                                    Add a Pattern
+                                </Button>
+                            </VStack>
+                        </Grid>
+                        <VStack minH="25vh" p={8} spacing={8}>
+                            {patternData.map(pattern => (
+                                <Pattern
+                                    key={pattern.id}
+                                    pattern={JSON.parse(pattern.pattern)}
+                                    meaning={pattern.meaning}
+                                />
+                            ))}
                         </VStack>
-                    </Grid>
-                    <VStack minH="25vh" p={8} spacing={8}>
-                        {patternData.map(pattern => (
-                            <Pattern
-                                key={pattern.id}
-                                pattern={JSON.parse(pattern.pattern)}
-                                meaning={pattern.meaning}
-                            />
-                        ))}
-                    </VStack>
-                </Box>
-            </ChakraProvider>
-        );
-    }
+                    </>
+                )}
+            </Box>
+        </ChakraProvider>
+    );
 }
 export default App;
