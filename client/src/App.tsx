b88493c9f6a342a3701f12ac1ada70a4b5299997
syntaxbullet <iviscripts@gmail.com>
2021-10-16 22:46:29 +0200

    typescript refactoring setup

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import * as React from "react"
+import {
+  ChakraProvider,
+  Box,
+  Grid,
+  theme,
+} from "@chakra-ui/react"
+import { ColorModeSwitcher } from "./components/ColorModeSwitcher"
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+      </Grid>
+    </Box>
+  </ChakraProvider>
+)
