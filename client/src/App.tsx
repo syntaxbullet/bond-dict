2c025d1e81fba488b2f99f5b1258eb5154101a89
syntaxbullet <iviscripts@gmail.com>
2021-10-17 14:27:44 +0200

    added prettier formatting following JS Standard Style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,30 @@
-import * as React from "react";
-import { useState, useEffect } from "react";
-import {
-  ChakraProvider,
-  Grid,
-  VStack,
-  Text,
-  theme,
-} from "@chakra-ui/react";
-import { ColorModeSwitcher } from "./components/ColorModeSwitcher";
-import { Hero } from "./components/Hero";
-import { Recorder } from "./components/Recorder";
+import * as React from 'react'
+import { useState, useEffect } from 'react'
+import { ChakraProvider, Grid, VStack, Text, theme } from '@chakra-ui/react'
+import { ColorModeSwitcher } from './components/ColorModeSwitcher'
+import { Hero } from './components/Hero'
+import { Recorder } from './components/Recorder'
 
 export const App = () => {
-  const [numberOfPatterns, setNumberOfPatterns] = useState(0);
-  const [patterns, setPatterns] = useState([]);
-  const [isRecording, setIsRecording] = useState(false);
+  const [numberOfPatterns, setNumberOfPatterns] = useState(0)
+  const [patterns, setPatterns] = useState([])
+  const [isRecording, setIsRecording] = useState(false)
   // create a useEffect to fetch the number of patterns
   useEffect(() => {
-    fetch("http://localhost:5000/patterns")
+    fetch('http://localhost:5000/patterns')
       .then((res) => res.json())
       .then((data) => {
-        setNumberOfPatterns(data.length);
-        setPatterns(data);
-      });
-  }, []);
+        setNumberOfPatterns(data.length)
+        setPatterns(data)
+      })
+  }, [])
   return (
     <ChakraProvider theme={theme}>
       <Grid minH="100vh">
-        <ColorModeSwitcher justifySelf="flex-end" margin="8"/>
+        <ColorModeSwitcher justifySelf="flex-end" margin="8" />
         <VStack spacing="8">
-          { !isRecording && <Hero setRecording={setIsRecording}/>}
-          { isRecording && <Recorder/>}
+          {!isRecording && <Hero setRecording={setIsRecording} />}
+          {isRecording && <Recorder />}
         </VStack>
         <VStack>
           {patterns.map((pattern: any) => (
@@ -43,5 +37,5 @@ export const App = () => {
         </VStack>
       </Grid>
     </ChakraProvider>
-  );
-};
+  )
+}
--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -1,13 +1,26 @@
-import * as React from "react";
-import { Heading, Text, Button, Box } from "@chakra-ui/react";
+import * as React from 'react'
+import { Heading, Text, Button, Box } from '@chakra-ui/react'
 import 'animate.css'
 const Hero = (props: any) => (
-    <>
-        <Box textAlign="center">
-        <Heading size="xl" className="animate__animated animate__fadeInDownBig">bond-dict</Heading>
-        <Text className="animate__animated animate__fadeInDown" style={{animationDelay: '1s'}}>some text</Text>
-        </Box>
-        <Button className="animate__animated animate__fadeInDown" style={{animationDelay: '1s'}} onClick={() => props.setRecording(true)}>Click me</Button>
-    </>
-);
-export { Hero };
+  <>
+    <Box textAlign="center">
+      <Heading size="xl" className="animate__animated animate__fadeInDownBig">
+        bond-dict
+      </Heading>
+      <Text
+        className="animate__animated animate__fadeInDown"
+        style={{ animationDelay: '1s' }}
+      >
+        some text
+      </Text>
+    </Box>
+    <Button
+      className="animate__animated animate__fadeInDown"
+      style={{ animationDelay: '1s' }}
+      onClick={() => props.setRecording(true)}
+    >
+      Click me
+    </Button>
+  </>
+)
+export { Hero }
